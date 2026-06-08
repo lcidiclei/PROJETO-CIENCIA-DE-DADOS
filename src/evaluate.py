55158dce04a1cf0278bee6aa26ecfb8dae192d99
Lucas Cidiclei <165520927+lcidiclei@users.noreply.github.com>
2026-06-08 15:50:15 -0300

    Rename function and improve plot formatting

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,12 +1,19 @@
 import matplotlib.pyplot as plt
 
 
-def plot_previsao(y_test, pred):
+def plot_resultados(y_test, pred):
 
-    plt.figure(figsize=(12,5))
+    plt.figure(figsize=(12, 5))
 
-    plt.plot(y_test.values[:200], label='Real')
-    plt.plot(pred[:200], label='Previsto')
+    plt.plot(
+        y_test.values[:200],
+        label='Real'
+    )
+
+    plt.plot(
+        pred[:200],
+        label='Previsto'
+    )
 
     plt.legend()
 
