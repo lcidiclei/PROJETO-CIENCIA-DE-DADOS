a1cc42614944456c901b6b6531c3cb1af829dd26
mole.py <58610661+baldini-dev@users.noreply.github.com>
2026-06-16 15:44:56 -0300

    correçao no url e melhoria no carregamento dos dados
    Updated dataset URL and improved dataset loading logic.

--- src/loader.py
+++ src/loader.py
@@ -3,25 +3,21 @@ import urllib.request
 import pandas as pd
 
 DATASET_PATH = "train.csv"
-URL_DATASET = ("https://www.kaggle.com/competitions/demand-forecasting-kernels-only/data?select=test.csv")
+URL_DATASET = ("https://www.kaggle.com/competitions/demand-forecasting-kernels-only/data?select=train.csv")
 
 def baixar_dataset():
-
-
-def carregar_dataset(caminho):
-    if os.path.exists(DATASET_PATH):
-        print("Dataset já existe.")
-        return
-      
     print("Baixando dataset...")
     urllib.request.urlretrieve(
         URL_DATASET,
         DATASET_PATH
     )
 
-    if not os.path.exists(caminho):
+def carregar_dataset(caminho):
+    if os.path.exists(caminho):
+        print("Dataset já existe.")
+    else:
         baixar_dataset()
-      
+
     df = pd.read_csv(caminho)
     print(f"Shape: {df.shape}")
     return df
